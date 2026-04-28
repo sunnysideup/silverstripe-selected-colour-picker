b7b93efbdf96e7b38dc48de6207c42b89839e28a
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-04-29 11:07:46 +1200

    FIX: post SS6 Fixes

--- src/Forms/SelectedColourPickerFormField.php
+++ src/Forms/SelectedColourPickerFormField.php
@@ -50,28 +50,32 @@ class SelectedColourPickerFormField extends TextField
     #[Override]
     public function validate(): ValidationResult
     {
+        $result = parent::validate();
         if ($this->limitedToOptions && $this->value && ! isset($this->source[$this->value])) {
-            $valid->addError(
+            $result->addError(
                 'Please selected from suggested options only',
                 'validation'
             );
 
-            return false;
         }
 
-        return true;
+        return $result;
     }
 
     #[Override]
     public function Field($properties = [])
     {
         $this->setAttribute('list', $this->ID() . '_List');
+        /**
+         * @deprecated FormField::Value() has been deprecated. It will be replaced by getFormattedValue() and getValue().
+         * See: https://docs.silverstripe.org/en/5/changelogs/5.4.0/#deprecated-api
+         */
         $this->setDescription(
             DBField::create_field(
                 'HTMLText',
                 SelectedColourPickerFormFieldSwatches::get_swatches_html(
                     $this->name,
-                    $this->value,
+                    $this->getValue(),
                     $this->source,
                     $this->isBgColour
                 )
